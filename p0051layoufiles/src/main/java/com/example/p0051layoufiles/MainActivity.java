66d74cef1fb52ed39a30c6c55d4c94a6cd6bbc86
mr3ero <mr3ero@vk.com>
2023-02-15 09:42:20 +0900

    1502

---
+++ p0051layoufiles/src/main/java/com/example/p0051layoufiles/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.p0051layoufiles;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
